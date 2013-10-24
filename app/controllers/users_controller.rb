040b1f9541ea0d2588410f58b21f4cc8d7aebf2e
Andrew Kofink <ajkofink@gmail.com>
2013-10-24 08:44:11 -0400

    Users may now be created and updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,42 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = 'User successfully created'
+      redirect_to @user
+    else
+      redirect_to :edit
+    end
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
   end
 
   def update
+    @user = User.find params[:id]
+    if @user.update user_params
+      redirect_to @user
+    else
+      render :edit
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit!
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,15 @@ require 'bcrypt'
 
 class User < ActiveRecord::Base
   include BCrypt
+  attr_accessor :password_confirmation
+  validates :password, confirmation: true
 
   def password
     @password ||= Password.new(password_hash)
   end
 
   def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_hash = @password
+    self.password_hash = Password.create(new_password)
   end
 
   def valid_password?(password)
