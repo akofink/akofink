d6d650109019d8098be53ebf61cc8f6ad4ba2e19
Andrew Kofink <ajkofink@gmail.com>
2013-10-24 08:02:37 -0400

    Add flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ include ApplicationHelper
 
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def current_session
+    @current_session ||= Session.current
+  end
 end
