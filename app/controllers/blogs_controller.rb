076b919e01e58ee2db771fe0f1fe4b7d084828af
Andrew Kofink <ajkofink@gmail.com>
2013-11-22 07:51:30 -0500

    Add edit and delete links to blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,7 @@
 class BlogsController < ApplicationController
   def index
     @title = 'Blog'
-    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    markdown
     @new_blog_post = Blog.new
   end
 
@@ -16,17 +16,43 @@ class BlogsController < ApplicationController
     if blog.delete
       log "Destroyed Blog Entry: #{blog}"
     end
+    flash[:info] = "Blog #{blog.id} destroyed."
     redirect_to root_path
   end
 
+  def edit
+    blog
+  end
+
+  def update
+    if blog.update(blog_params)
+      flash[:success] = "Post successfully updated."
+      redirect_to blog
+    else
+      flash[:warning] = "Cannot save post."
+      redirect_back
+    end
+  end
+
+  def show
+    blog
+    markdown
+  end
+
   private
+
+  def markdown
+    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  end
 
   def blog_params
     params.require(:blog).permit(:content, :title)
   end
 
   def blog
-    @blog ||= (params[:id] && Blog.find(params[:id])) || Blog.new(blog_params)
+    @blog ||= (params[:id] && Blog.find(params[:id])) ||
+      Blog.find(blog_params) ||
+      Blog.new(blog_params)
   end
 
   def action_allowed?
